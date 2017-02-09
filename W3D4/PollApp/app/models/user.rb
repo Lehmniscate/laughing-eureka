c0c2de0d682c337fea899aa19fb98b199dd01a86
Lehmniscate <mchlhtn@gmail.com>
2017-02-09 15:59:24 -0800

    added a User#completed_polls method using SQL

--- W3D4/PollApp/app/models/user.rb
+++ W3D4/PollApp/app/models/user.rb
@@ -20,4 +20,28 @@ class User < ActiveRecord::Base
     primary_key: :id,
     foreign_key: :user_id,
     class_name: :Response
+
+  # def completed_polls
+  #   Poll.joins(:questions)
+  #   .joins()
+  #   .group(:poll_id)
+  #   .having("COUNT(questions.id) = COUNT(r.question_id)")
+  # end
+
+  def completed_polls_with_sql
+    Poll.find_by_sql([<<-SQL, id])
+      SELECT polls.*
+      FROM polls
+      JOIN questions ON questions.poll_id = polls.id
+      LEFT JOIN (
+          SELECT *
+          FROM answer_choices
+          JOIN responses ON answer_choices.id = responses.answer_choice_id
+          WHERE responses.user_id = ?
+        ) AS user_responses ON user_responses.question_id = questions.id
+      GROUP BY polls.id
+      HAVING COUNT(questions.id) = COUNT(user_responses.question_id)
+    SQL
+  end
+
 end
