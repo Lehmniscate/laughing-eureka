86e0fd7f61b28a2ab355251357298193e2d891b5
Lehmniscate <mchlhtn@gmail.com>
2017-02-10 15:18:17 -0800

    added searchable module to allo finding objects using a 'where' method

--- W3D5/lib/02_searchable.rb
+++ W3D5/lib/02_searchable.rb
@@ -3,10 +3,15 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_line = params.keys.map {|k| "#{k} = ?"}.join(" AND ")
+    parse_all DBConnection.execute(<<-SQL, params.values)
+      SELECT *
+      FROM #{table_name}
+      WHERE #{where_line}
+    SQL
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
