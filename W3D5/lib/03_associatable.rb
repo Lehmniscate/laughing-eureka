36a262f38142ed445458fcd8c364b59b9ea240dd
Lehmniscate <mchlhtn@gmail.com>
2017-02-10 16:39:12 -0800

    satisfy specs for active record lite

--- W3D5/lib/03_associatable.rb
+++ W3D5/lib/03_associatable.rb
@@ -10,41 +10,62 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    @class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    @foreign_key = options[:foreign_key] || "#{name}_id".to_sym
+    @primary_key = options[:primary_key] || :id
+    @class_name = options[:class_name] || name.to_s.camelcase
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    @foreign_key = options[:foreign_key] || "#{self_class_name.to_s.singularize.downcase}_id".to_sym
+    @primary_key = options[:primary_key] || :id
+    @class_name = options[:class_name] || name.to_s.singularize.camelcase
   end
 end
 
 module Associatable
   # Phase IIIb
-  def belongs_to(name, options = {})
-    # ...
+  def belongs_to(name, opt = {})
+    assoc_options[name] = BelongsToOptions.new(name, opt)
+    define_method(name) do
+      options = self.class.assoc_options[name]
+
+      owner_key = options.primary_key
+      f_k = self.send(options.foreign_key)
+
+      options.model_class.where(owner_key => f_k).first
+    end
   end
 
-  def has_many(name, options = {})
-    # ...
+  def has_many(name, opt = {})
+    assoc_options[name] = HasManyOptions.new(name, self, opt)
+    define_method(name) do
+      options = self.class.assoc_options[name]
+
+      f_k = options.foreign_key
+      owner_key = self.send(options.primary_key)
+      
+      options.model_class.where(f_k => owner_key)
+    end
   end
 
   def assoc_options
-    # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
+    @assoc_options
   end
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
--- W3D5/lib/04_associatable2.rb
+++ W3D5/lib/04_associatable2.rb
@@ -5,6 +5,22 @@ module Associatable
   # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
-    # ...
+    define_method(name) do
+      through_options = self.class.assoc_options[through_name]
+      source_options = through_options.model_class.assoc_options[source_name]
+
+      key = self.attributes[through_options.foreign_key]
+
+      through_table = through_options.model_class.table_name
+      source_table = source_options.model_class.table_name
+      r = source_options.model_class.parse_all DBConnection.execute(<<-SQL, key)
+        SELECT #{source_table}.*
+        FROM #{source_table}
+        JOIN #{through_table} ON #{source_table}.#{source_options.primary_key} = #{through_table}.#{source_options.foreign_key}
+        WHERE #{through_table}.#{through_options.primary_key} = ?
+      SQL
+
+      r.first
+    end
   end
 end
